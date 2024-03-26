6160b33292316f364c68ff195f8c2b95a564dfe1
Dhyey Vyas <48405523+Dhyey17@users.noreply.github.com>
2024-03-26 19:03:30 +0530

    Initial Commit

--- ayusadhana/accounts/models.py
+++ ayusadhana/accounts/models.py
@@ -1,3 +1,55 @@
+import uuid
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser, PermissionsMixin):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", 'Admin'
+        USER = "USER", 'User'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
+    role = models.CharField(max_length=25, choices=Role.choices, default=Role.ADMIN)
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(max_length=12, unique=True)
+    first_name = models.CharField(max_length=255)
+    otp = models.CharField(max_length=6)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = [phone_number]
+    USERNAME_FIELD = [phone_number]
+
+
+class CustomUserManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(role=User.Role.USER)
+
+
+class CustomAdminManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(role=User.Role.ADMIN)
+
+
+class CustomUser(User):
+    objects = CustomUserManager()
+
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        self.role = User.Role.USER
+        return super().save(*args, **kwargs)
+
+
+class CustomAdmin(User):
+    objects = CustomAdminManager()
+
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        self.role = User.Role.ADMIN
+        return super().save(*args, **kwargs)
---
+++ ayusadhana/accounts/utils.py
@@ -0,0 +1,67 @@
+import datetime
+import random
+import uuid
+import pytz
+import requests
+from dotenv import load_dotenv
+from jwt import ExpiredSignatureError, encode, decode
+from rest_framework.authentication import get_authorization_header
+
+load_dotenv()
+
+
+def generate_otp():
+    ts = datetime.datetime.now(tz=pytz.UTC)
+    return random.randint(100000, 999999), ts
+
+
+def otp_sender(number, otp):
+    api_key = "3bf58d9f-0e91-11ee-addf-0200cd936042"
+    phone_number = f'+91{number}'
+
+    url = f'https://2factor.in/API/V1/{api_key}/SMS/{phone_number}/{otp}/FreskoOTP'
+    requests.get(url)
+
+
+def create_access_token(user_id, username, role) -> str:
+    data = {
+        "id": str(user_id),
+        "username": username,
+        "role": role
+    }
+    expire_time = datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=5)
+    token_payload = {
+        "data": data,
+        "exp": expire_time,
+        "iat": datetime.datetime.now(datetime.UTC)
+    }
+    secret = "access secret"
+    try:
+        access_token = encode(token_payload, secret, algorithm="HS256")
+        return access_token
+    except Exception as e:
+        raise ValueError("Unable to create access token") from e
+
+
+def decode_access_token(token) -> dict:
+    try:
+        payload = decode(token, "access secret", algorithms="HS256")
+        payload["data"]["id"] = uuid.UUID(payload["data"]["id"])
+        return payload["data"]
+
+    except ExpiredSignatureError:
+        raise ValueError("Token has expired")
+
+    except Exception as e:
+        raise ValueError("Unable to decode access token") from e
+
+
+def authorize_user(request) -> dict:
+    header_auth = get_authorization_header(request).split()
+    if header_auth and len(header_auth) == 2:
+        token = header_auth[1].decode("utf-8")
+        data = decode_access_token(token)
+
+        if data:
+            return data
+    raise ValueError("Unauthorized")
